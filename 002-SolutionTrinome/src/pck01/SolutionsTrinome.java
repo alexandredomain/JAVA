bbad438d15cf7930920ef6b875c6b87d257423ed
Alexandre Domain <domain.alexandre@gmail.com>
2016-01-15 18:29:48 +0100

    Ajout de l'input

--- 002-SolutionTrinome/src/pck01/SolutionsTrinome.java
+++ 002-SolutionTrinome/src/pck01/SolutionsTrinome.java
@@ -1,12 +1,14 @@
 package pck01;
+import java.util.Scanner;
 
 /**
  * @author Alexandre Domain
  */
 public class SolutionsTrinome {
-
+    
     public static void main(String[] args) {
-        System.out.println(solTrinome(1,1,1)) ;
+        System.out.println("ax²+bx+c=0");
+        System.out.println(solTrinome(inputDouble("entrez a :"),inputDouble("entrez b :"),inputDouble("entrez c :"))) ;
     }
     
     public static String solTrinome(double a, double b, double c) {
@@ -20,6 +22,24 @@ public class SolutionsTrinome {
         else {
             return("Racine complexes : " + -b/(2*a) + -Math.sqrt(-d)/(2*a) + "i" + " et " + -b/(2*a) + "+" + +Math.sqrt(-d)/(2*a) + "i");
         }
+    }
+    
+    /*
+    public static String inputString(String demande){
+      Scanner sc = new Scanner(System.in);
+      System.out.println(demande);
+      //On vide la ligne avant d'en lire une autre
+      sc.nextLine();
+      String str = sc.nextLine();      
+      return str;
+    }
+    */
+    
+    public static double inputDouble(String demande){
+      Scanner sc = new Scanner(System.in);
+      System.out.println(demande);
+      double nb = sc.nextDouble();    
+      return nb;
     }
     
 }
